b9c3d5e53dc2f63518373d479d975196f6e22c00
DokoBhai <159497103+DokoBhai@users.noreply.github.com>
2025-12-27 23:57:12 +0000

    Add setup files for unix systems
    -Added unix-libraries.sh
    -Added unix-haxeflixel.sh

---
+++ setup/haxeflixel.sh
@@ -0,0 +1,12 @@
+haxelib install lime 8.1.1
+haxelib install openfl 9.3.2
+haxelib install flixel 5.6.2
+
+haxelib run lime setup flixel
+haxelib run lime setup
+
+haxelib install flixel-tools 1.5.1
+haxelib run flixel-tools setup
+
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-ui 2.5.1
---
+++ setup/unix-libraries.sh
@@ -0,0 +1,13 @@
+haxelib set lime 8.1.1
+haxelib set openfl 9.3.2
+haxelib set flixel 5.6.2
+haxelib set flixel-addons 3.2.2
+haxelib set flixel-ui 2.5.0
+
+haxelib install away3d 5.0.9
+haxelib install hxvlc 1.3.0
+haxelib install tjson 1.4.0
+haxelib git hscript-improved-dev https://github.com/CodenameCrew/hscript-improved.git codename-dev
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
+haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
